6c62686fe678a48849eb7b6463b6bb6371f07075
Keerti Vardhan Singh <aryan3800chauhan@gmail.com>
2025-10-18 01:46:21 +0530

    Time: 0 ms (100.00%) | Memory: 15.1 MB (85.74%) - LeetSync

--- 532-k-diff-pairs-in-an-array/k-diff-pairs-in-an-array.cpp
+++ 532-k-diff-pairs-in-an-array/k-diff-pairs-in-an-array.cpp
@@ -2,26 +2,28 @@ class Solution {
 public:
     int findPairs(vector<int>& nums, int k) {
         sort(nums.begin(),nums.end());
-        int n= nums.size();
-        int count =0;
         int i=0;
         int j=1;
+        int count =0;
+
+        int n= nums.size();
         while(j<n){
             if(i==j || nums[j]-nums[i]<k){
                 j++;
 
             }else if(nums[j]-nums[i]>k){
-                i++;
+i++;
 
             }else{
                 count++;
                 i++;
                 j++;
+                
                 while(j<n && nums[j]==nums[j-1])j++;
 
             }
         }
         return count ;
-        
+
     }
 };
