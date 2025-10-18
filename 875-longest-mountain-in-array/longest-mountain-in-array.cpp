0d48af67c481700912d7c342dc9130a1357015e7
Keerti Vardhan Singh <aryan3800chauhan@gmail.com>
2025-10-19 00:44:54 +0530

    Time: 0 ms (100.00%) | Memory: 22.4 MB (56.41%) - LeetSync

---
+++ 875-longest-mountain-in-array/longest-mountain-in-array.cpp
@@ -0,0 +1,38 @@
+class Solution {
+public:
+    int longestMountain(vector<int>& arr) {
+
+        int n = arr.size();
+        int ans  =0;
+        int i=1;
+
+        while(i < n-1){
+
+
+            if(arr[i-1]<arr[i] && arr[i]>arr[i+1]){
+                int right = i+1;
+int left = i-1;
+
+while(left>0 && arr[left-1]<arr[left])
+    left--;
+
+
+while(right<n-1 && arr[right]>arr[right+1] )
+    right++;
+
+
+ans = max(ans,right-left+1);
+i=right;
+
+
+
+            }else{
+                i++;
+
+            }
+        }
+
+        return ans;
+
+    }
+};
