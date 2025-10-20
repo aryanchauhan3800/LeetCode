113c71a5fe46d882c45b8ecf3729be9c615230ad
Keerti Vardhan Singh <aryan3800chauhan@gmail.com>
2025-10-21 00:04:06 +0530

    Time: 8 ms (81.86%) | Memory: 29.1 MB (65.90%) - LeetSync

--- 475-heaters/heaters.cpp
+++ 475-heaters/heaters.cpp
@@ -1,23 +1,20 @@
 class Solution {
 public:
     int findRadius(vector<int>& houses, vector<int>& heaters) {
-        sort(houses.begin(),houses.end());
-        sort(heaters.begin(),heaters.end());
-
         int ans =0;
         int n= heaters.size();
-int i=0;
+        int i=0;
+        sort(heaters.begin(),heaters.end());
+        sort(houses.begin(),houses.end());
+for(int house : houses){
+        while(i<n-1 && abs(heaters[i+1]- house) <= abs(heaters[i]-house))
+        i++;
 
-        for(int house : houses){
+        ans = max(ans , abs(heaters[i]-house));
 
-            while(i < n-1 && abs(heaters[i+1]-house) <= abs(heaters[i]- house))
-            i++;
-            ans = max(ans , abs(heaters[i]-house));
+}
+         return  ans ;
 
-        }
-        return ans ;
-
-        
-    
     }
+   
 };
