968919336a65f356968b6e0a54f2c7bb18d05bb3
Keerti Vardhan Singh <aryan3800chauhan@gmail.com>
2025-10-26 01:47:31 +0530

    Time: 1330 ms (5.26%) | Memory: 162.8 MB (5.70%) - LeetSync

--- 959-3sum-with-multiplicity/3sum-with-multiplicity.cpp
+++ 959-3sum-with-multiplicity/3sum-with-multiplicity.cpp
@@ -1,21 +1,19 @@
 class Solution {
 public:
     int threeSumMulti(vector<int>& arr, int target) {
-        int n= arr.size();
         long long  ans =0;
-      const int mod = 1e9 + 7;
+        int n = arr.size();
+        const int mod = 1e9 + 7;
 
-      for(int i=0;i<n;i++){
-        unordered_map<int,int> m;
-        for(int j=i+1;j<n;j++){
-            int k= target- arr[i]- arr[j];
-            if(m.find(k)!=m.end()) ans +=m[k];
-            m[arr[j]]++;
+        for(int i=0;i<n;i++){
+            unordered_map<int ,int> m;
+            for(int j=i+1;j<n;j++){
+                int k = target - arr[i]- arr[j];
+                if(m.find(k)!=m.end()) ans +=m[k];
+                m[arr[j]]++;
 
+            }
         }
-      }
-
         return ans % mod;
-
     }
 };
