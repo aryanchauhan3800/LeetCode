fcf641cf28d59af8e52bb06331c81bef9df52aa2
Keerti Vardhan Singh <aryan3800chauhan@gmail.com>
2025-10-29 21:32:19 +0530

    Time: 11 ms (70.90%) | Memory: 41.6 MB (58.61%) - LeetSync

---
+++ 853-most-profit-assigning-work/most-profit-assigning-work.cpp
@@ -0,0 +1,27 @@
+class Solution {
+public:
+    int maxProfitAssignment(vector<int>& difficulty, vector<int>& profit, vector<int>& worker) {
+        int n=  difficulty.size();
+     vector <pair<int,int>> jobs;
+     for(int i=0;i<n;i++){
+        jobs.push_back({difficulty[i],profit[i]});
+
+     }
+     sort(jobs.begin(),jobs.end());
+     sort(worker.begin(),worker.end());
+
+     int ans =0;
+     int best =0;
+     int i=0;
+     for(int w : worker){
+        while(i<n && jobs[i].first<= w){
+            best = max(best , jobs[i].second);
+            i++;
+
+        }
+        ans+=best;
+     }
+     return ans ;
+
+    }
+};
