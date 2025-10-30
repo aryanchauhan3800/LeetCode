5f7cd6f6880073e7f7fbb3eeb9fe5554dce1cda5
Keerti Vardhan Singh <aryan3800chauhan@gmail.com>
2025-10-31 00:06:46 +0530

    Time: 51 ms (5.13%) | Memory: 60.6 MB (5.00%) - LeetSync

--- 22-generate-parentheses/generate-parentheses.cpp
+++ 22-generate-parentheses/generate-parentheses.cpp
@@ -1,39 +1,48 @@
 class Solution {
 public:
-    vector<string> result;
+vector<string> result;
+bool isValid(string &str){
+    int count =0;
 
-    bool isValid(string &str) {
-        int count = 0;
-        for (char &ch : str) {
-            if (ch == '(')
-                count++;
-            else
-                count--;
-            if (count < 0) return false; // invalid prefix
-        }
-        return count == 0; // ✅ moved outside loop
-    }
+    for(char &ch : str){
 
-    void solve(string curr, int n) {
-        if (curr.length() == 2 * n) {
-            if (isValid(curr)) {
-                result.push_back(curr);
-            }
-            return;
+        if(ch=='('){
+            count++;
+
+        }else{
+            count--;
+            if(count<0) return false;
+
         }
 
-        curr.push_back('(');
-        solve(curr, n);
-        curr.pop_back();
-
-        curr.push_back(')');
-        solve(curr, n);
-        curr.pop_back();
     }
+    return count==0;
+
+}
+void solve(string curr,int n){
+    if(curr.length()==2*n){
+        if(isValid(curr)){
+            result.push_back(curr);
+
+        }
+        return ;
+
+
+    }
+    curr.push_back(')');
+    solve(curr,n);
+    curr.pop_back();
+    curr.push_back('(');
+    solve(curr,n);
+    curr.pop_back();
+
+
+}
 
     vector<string> generateParenthesis(int n) {
-        string curr = "";
-        solve(curr, n);
+        string curr ="";
+        solve(curr,n);
         return result;
+
     }
 };
