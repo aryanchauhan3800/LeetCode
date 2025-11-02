017341a1193e8e651c7fde05edcbe86228015650
Keerti Vardhan Singh <aryan3800chauhan@gmail.com>
2025-11-02 13:23:47 +0530

    Time: 91 ms (7.53%) | Memory: 72.3 MB (5.88%) - LeetSync

---
+++ 39-combination-sum/combination-sum.cpp
@@ -0,0 +1,27 @@
+class Solution {
+public:
+vector<vector<int>> result;
+
+void solve(int i,vector<int>& candidates,vector<int> temp,int target){
+
+if(target==0){
+    result.push_back(temp);
+return ;
+}
+ if (i >= candidates.size() || target < 0) return;
+temp.push_back(candidates[i]);
+solve(i,candidates,temp,target-candidates[i]);
+temp.pop_back();
+solve(i+1,candidates,temp,target);
+
+
+
+}
+    vector<vector<int>> combinationSum(vector<int>& candidates, int target) {
+        vector<int> temp;
+        solve(0,candidates,temp,target);
+        return result;
+
+        
+    }
+};
